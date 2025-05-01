d6baf935cd86b96c55305e331e8cc917b443310d
Bono Nonchev <bono.nonchev@gmail.com>
2025-05-01 10:49:54 -0400

    added custom colors

--- config.py
+++ config.py
@@ -2,12 +2,16 @@ import dataclasses
 import datetime
 import tomllib
 from enum import Enum
+from functools import cached_property
 from typing import Dict, List, Iterable, Any
+
+from colour import Color
 
 
 class EventType(Enum):
-    GENERAL = None
+    GENERAL = 'None'
     BIRTHDAY = "birthday"
+    DEATH = "death"
 
 
 @dataclasses.dataclass
@@ -24,11 +28,11 @@ class Events:
     @property
     def all(self) -> Iterable[Event]:
         for name, prefs in self.events_data.items():
-            yield Event(name, prefs["date"], EventType(prefs.get("type")))
+            yield Event(name, prefs["date"], EventType(prefs.get("type", 'None')))
 
 
 class PhaseType(Enum):
-    GENERAL = None
+    GENERAL = 'None'
     JOB = "job"
 
 
@@ -47,7 +51,7 @@ class Phases:
     @property
     def all(self) -> Iterable[Phase]:
         for name, prefs in self.phases_data.items():
-            yield Phase(name, prefs["from_date"], prefs["to_date"], PhaseType(prefs.get("layer")))
+            yield Phase(name, prefs["from_date"], prefs["to_date"], PhaseType(prefs.get("layer", 'None')))
 
 
 class Config:
@@ -61,12 +65,30 @@ class Config:
     @property
     def phases(self) -> Phases: return Phases(self.data['phases'])
 
+    @cached_property
+    def event_colors(self) -> Dict[EventType, Color]:
+        return dict(
+            (EventType(event_type), Color(color_spec).rgb)
+            for event_type, color_spec in self.data["event-colors"].items())
+
+    @cached_property
+    def phase_colors(self) -> Dict[PhaseType, Color]:
+        return dict(
+            (PhaseType(phase_type), Color(color_spec).rgb)
+            for phase_type, color_spec in self.data["phase-colors"].items())
+
 
 if __name__ == "__main__":
-    config = Config("config.toml")
+    config = Config("my_config.toml")
 
     for event in config.events.all:
         print(event)
 
     for phase in config.phases.all:
         print(phase)
+
+    for event_type, color in config.event_colors.items():
+        print(event_type, color)
+
+    for phase_type, color in config.phase_colors.items():
+        print(phase_type, color)
